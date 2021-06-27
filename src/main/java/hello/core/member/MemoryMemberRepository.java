e43a6ca3c91d0fe9ea29e05461afb617da95593a
정 수미(Su-Mi Jeong) <suuumi08@gmail.com>
2021-06-27 15:18:43 +0900

    spring 핵심원리2강

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+	private static Map<Long, Member> store = new HashMap<>();
+	
+	public void save(Member member) {
+		store.put(member.getId(), member);
+	}
+
+	public Member findById(Long memberId) {
+		return store.get(memberId);
+	}
+
+}
